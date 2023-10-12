f2e085f7e41b0c10de81594aa8b0c40cfc9e7ae6
Blackarrow299 <yassine.goumni@gmail.com>
2023-10-12 12:06:22 +0100

    (task 4) Refactor the BaseModel constructor

--- models/base_model.py
+++ models/base_model.py
@@ -2,19 +2,35 @@
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        
+    """Base class"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a BaseModel object."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(
+                            value, '%Y-%m-%dT%H:%M:%S.%f').date())
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
     def __str__(self):
+        """Return a string representation of the object."""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
+
     def save(self):
+        """Update the 'updated_at' attribute with the current date and time."""
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
+        """Return a dictionary representation of the object."""
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = self.__class__.__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
