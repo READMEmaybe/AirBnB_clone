80ca689ab9c79ae6b02b9eeccf8c47340c44f097
Blackarrow299 <yassine.goumni@gmail.com>
2023-10-12 15:47:33 +0100

    (task 6) create HBNBCommand class

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import cmd
+from turtle import bye
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, args):
+        'Quit command to exit the program'
+        return True
+
+    def do_EOF(self, args):
+        'Quit command to exit the program'
+        return True
+
+    def postcmd(self, stop, line):
+        if line == 'EOF':
+            return True
+        return stop
+
+    def emptyline(self):
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
